fa204b0fece21078e96bc1983da2f03de265418e
Sourav Sharma <142586690+544sourav@users.noreply.github.com>
2026-02-03 16:19:01 +0530

    Time: 42 ms (18.35%), Space: 51.2 MB (99.93%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxs = 0;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            if(i>maxs) return false;
+            maxs = max(maxs,i+nums[i]);
+            if(maxs>=n-1) return true;
+        }
+        return false;
+    }
+};
