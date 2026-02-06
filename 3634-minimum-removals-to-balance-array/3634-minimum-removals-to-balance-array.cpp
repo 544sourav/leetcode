53eef4297df561c5acf22a489cd850a07ec92627
Sourav Sharma <142586690+544sourav@users.noreply.github.com>
2026-02-06 10:30:07 +0530

    Time: 40 ms (29.36%), Space: 105.2 MB (14.24%) - LeetHub

--- 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
@@ -2,14 +2,23 @@ class Solution {
 public:
     int minRemoval(vector<int>& nums, int k) {
         int n = nums.size();
-        int mini = INT_MAX;
+        // int mini = INT_MAX;
+        int maxi = INT_MIN;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++){
-            long long x = 1LL* nums[i]*k;
-            int idx = upper_bound(nums.begin(),nums.end(),x)-nums.begin();
-            // cout<<idx<<endl;
-             mini = min((i+(n-idx)),mini);
+        // for(int i=0;i<n;i++){
+        //     long long x = 1LL* nums[i]*k;
+        //     int idx = upper_bound(nums.begin(),nums.end(),x)-nums.begin();
+        //     // cout<<idx<<endl;
+        //      mini = min((i+(n-idx)),mini);
+        // }
+        int left =0;
+        for(int right =0;right<n;right++){
+
+           while ((long long)nums[right] > (long long)k * nums[left]) {
+                left++;
+            }
+            maxi = max(maxi,right-left+1);
         }
-        return mini;
+        return n-maxi;
     }
 };
