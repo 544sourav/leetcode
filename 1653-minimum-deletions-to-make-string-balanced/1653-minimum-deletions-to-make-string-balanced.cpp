66f527c902443dcea19e2b1388435c10c0bcdf98
Sourav Sharma <142586690+544sourav@users.noreply.github.com>
2026-02-07 09:45:24 +0530

    Time: 15 ms (82.84%), Space: 25.6 MB (54.88%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int bCount = 0;
-        int deletions = 0;
+        // int bCount = 0;
+        // int deletions = 0;
 
-        for (char c : s) {
-            if (c == 'b') {
-                bCount++;
-            } else { 
-                deletions = min(deletions + 1, bCount);
-            }
+        // for (char c : s) {
+        //     if (c == 'b') {
+        //         bCount++;
+        //     } else { 
+        //         deletions = min(deletions + 1, bCount);
+        //     }
+        // }
+        // return deletions;
+        int cnta =0;
+        int cntb =0;
+        for(auto x:s){
+            if(x =='a')cnta++;
         }
-        return deletions;
+        int mini = cnta;
+        for(auto x:s){
+            if(x=='a')cnta--;
+            else cntb++;
+            mini = min(cnta+cntb ,mini);
+        }
+        return mini;
+
     }
 };
