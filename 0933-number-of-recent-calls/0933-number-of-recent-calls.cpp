48d6b1a67931a6f5d40dc2fb102402770ee14d05
Sourav Sharma <142586690+544sourav@users.noreply.github.com>
2026-02-10 11:17:00 +0530

    Time: 26 ms (20.33%), Space: 63.6 MB (99.26%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.cpp
@@ -0,0 +1,19 @@
+class RecentCounter {
+private:
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+public:
+    RecentCounter() {
+      
+    }
+    
+    int ping(int t) {
+        
+        while (!pq.empty() && pq.top() < t - 3000) {
+            pq.pop();
+        }
+
+        pq.push(t);
+        return pq.size();
+    }
+};
