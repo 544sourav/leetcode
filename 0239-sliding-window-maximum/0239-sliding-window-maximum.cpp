c15fa341335d6e3c6cef18bd0e4b5e4dfe01d83e
Sourav Sharma <142586690+544sourav@users.noreply.github.com>
2026-02-10 10:28:02 +0530

    Time: 1946 ms (5.06%), Space: 142.7 MB (20.95%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> nge(n, n);   
+        stack<int> st;
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && nums[st.top()] <= nums[i])
+                st.pop();
+            if (!st.empty()) nge[i] = st.top();
+            st.push(i);
+        }
+
+        for(auto x:nge) cout<<x<<" ";
+        cout<<endl;
+        vector<int> ans;
+        int j = 0;
+        for (int i = 0; i <= n - k; i++) {
+            if (j < i) j = i; 
+
+            
+            while (nge[j] < i + k)
+                j = nge[j];
+
+            ans.push_back(nums[j]);
+        }
+
+        return ans;
+    }
+};
