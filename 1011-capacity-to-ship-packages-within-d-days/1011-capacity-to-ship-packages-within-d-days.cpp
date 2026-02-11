858ad6a363e203a242378f6b62eb72d20e292a00
Sourav Sharma <142586690+544sourav@users.noreply.github.com>
2026-02-11 12:08:53 +0530

    Time: 16 ms (20.47%), Space: 35.2 MB (19.19%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
     int shipWithinDays(vector<int>& w, int d) {
-        int lo = *max_element(w.begin(),w.end());
-        int hi = accumulate(w.begin(),w.end(),0);
-        int ans=-1;
-        while(lo<=hi){
-            int mid = lo+(hi-lo)/2;
-            int sum =0;
-            int cnt =0;
-            for(auto wt:w){
-                if(sum + wt >mid){
-                    cnt++;
+        int lo = *max_element(w.begin(), w.end());
+        int hi = accumulate(w.begin(), w.end(), 0);
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            int days = 1;
+            int sum = 0;
+
+            for (int wt : w) {
+                if (sum + wt > mid) {
+                    days++;
                     sum = wt;
+                } else {
+                    sum += wt;
                 }
-                else{
-                    sum+=wt;
-                }
+
+                if (days > d) break;
             }
-            if(cnt<=d-1){
-                ans = mid;
-                hi = mid-1;
-            }
-            else{
-                lo=mid+1;
+
+            if (days <= d) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
             }
         }
-        return ans;
+
+        return lo;
     }
 };
