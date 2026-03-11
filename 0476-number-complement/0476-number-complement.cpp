f3c13e94959365eb5b89820ad84f6df2008131e2
Sourav Sharma <142586690+544sourav@users.noreply.github.com>
2026-03-11 08:03:10 +0530

    Time: 0 ms (100%), Space: 8.1 MB (14.98%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findComplement(int n) {
+         if(n==0)return 1;
+        string s = bitset<32>(n).to_string();
+        string r="";
+        int i=0;
+        while(s[i]=='0'){
+            i++;
+        }
+        for(i;i<s.size();i++){
+            if(s[i]=='1')r+='0';
+            else r+='1';
+        }
+     
+        int ans =0;
+        int  j =r.size()-1;
+        for(int i=0;i<r.size();i++){
+
+            ans += (r[i]-'0')*(1<<j);
+            j--;
+        }
+        return ans;
+    }
+};
