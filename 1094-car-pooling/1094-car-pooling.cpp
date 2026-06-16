52a768b4d892d0185a62b554282e8d17c2e6a2c5
Sourav Sharma <142586690+544sourav@users.noreply.github.com>
2026-06-16 17:08:24 +0530

    Time: 0 ms (100%), Space: 14.1 MB (95.75%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        int diff[1001] = {0};
+        sort(trips.begin(), trips.end(),
+             [](auto &a, auto &b) {
+                 return a[1] < b[1];
+             });
+
+        priority_queue<
+            pair<int,int>,
+            vector<pair<int,int>>,
+            greater<pair<int,int>>
+        > pq;
+
+        int cur = 0;
 
         for (auto &v : trips) {
             int passengers = v[0];
-            int start = v[1];
-            int end = v[2];
+            int from = v[1];
+            int to = v[2];
 
-            diff[start] += passengers;
-            diff[end]   -= passengers;
-        }
+            while (!pq.empty() && pq.top().first <= from) {
+                cur -= pq.top().second;
+                pq.pop();
+            }
 
-        int curr = 0;
-        for (int i = 0; i <= 1000; i++) {
-            curr += diff[i];
-            if (curr > capacity) return false;
+            cur += passengers;
+            if (cur > capacity) return false;
+
+            pq.push({to, passengers});
         }
 
         return true;
