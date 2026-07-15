ea1f1d5a3bc5b018ecc387509e7ae971933edb25
Sourav Sharma <142586690+544sourav@users.noreply.github.com>
2026-07-15 08:16:24 +0530

    Time: 0 ms (100%), Space: 7.8 MB (80.58%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,36 +1,10 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        vector<int> a(n);
-        for(int i = 0; i < n; i++) {
-            a[i] = i + 1;
+        int w=0;
+        for(int i =1;i<=n;i++){
+            w=(w+k)%i;
         }
-
-        int rem = n;
-        int i = 0;
-
-        while(rem != 1) {
-            int t = k - 1;
-
-            while(t > 0) {
-                i %= n;
-                if(a[i] == -1) {
-                    i++;
-                    continue;
-                }
-                i++;
-                t--;
-            }
-
-            while(a[i % n] == -1)
-                i++;
-
-            i %= n;
-            a[i] = -1;
-            rem--;
-            i++;
-        }
-
-        return *max_element(a.begin(), a.end());
+        return w+1;
     }
 };
