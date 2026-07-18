4ddda1a80de8eb1dc31571fb249b2c7010d941d1
Sourav Sharma <142586690+544sourav@users.noreply.github.com>
2026-07-18 08:50:32 +0530

    Time: 0 ms (100%), Space: 7.9 MB (8.44%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    string to_binary(int n){
-        string ans ="";
-        while(n>0){
-            ans += to_string(n%2);
-            n/=2;
-        }
-        return ans;
-    }
     bool hasAlternatingBits(int n) {
-        string binary = to_binary(n);
-        for(int i=1;i<binary.size();i++){
-            if(binary[i]==binary[i-1])return false;
-        }
-        return true;
+      string binary =  bitset<32>(n).to_string();
+      int i=0;
+      while(binary[i]!='1'){
+        i++;
+      }
+      while(i<binary.size()){
+        if(binary[i]==binary[i+1])return false;
+        i++;
+      }
+      return true;
     }
 };
